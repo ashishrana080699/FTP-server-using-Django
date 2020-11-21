cceb955e230f1eb7215a4c86be96871cce78738e
Ashish Rana <aashishrana269@gmail.com>
2020-11-21 21:07:58 +0530

    Update views.py

--- Info_Diems/views.py
+++ Info_Diems/views.py
@@ -12,25 +12,6 @@ def info(request):
     return render(request,"info.html")
 
 def info_upload(request):
-    department=request.POST["department"]
-    category=request.POST["category"]
-    startdate=request.POST["startdate"]
-    enddate=request.POST["enddate"]
-    duration=request.POST["duration"]
-    participants=request.POST["participants"]
-    year=request.POST["year"]
-    semister=request.POST["semister"]
-    facultyname=request.POST["facultyname"]
-    email=request.POST["email"]
-    phone=request.POST["phone"]
-    message=request.POST["message"]
-    if request.method=='POST':
-        myfile=request.FILES["file"]
-        fs=FileSystemStorage()
-        fs.save(myfile.name, myfile)
-
-    obj=Information(department=department,category=category,startdate=startdate,enddate=enddate,duration=duration,participants=participants,year=year,semister=semister,facultyname=facultyname,email=email,phone=phone,myfile=myfile.name,message=message)
-    obj.save()
 
     return render(request,"home.html")
 
@@ -39,18 +20,4 @@ def search(request):
     return render(request,"search.html")
 
 def find(request):
-    departmant=request.POST["department"]
-    year=request.POST["year"]
-    semister=request.POST["semister"]
-    category=request.POST["category"]
-    data=Information.objects.filter(department__iexact=departmant).filter(year__iexact=year).filter(semister__iexact=semister).filter( category__iexact=category)
-    
-    path=[] 
-    name=[]
-    if len(data)!=0:
-        for value in data:
-            path.append('/media/'+value.myfile)
-        for obj in data:
-            name.append(obj.myfile)
-    print(path)
     return render(request,"search.html",{'path':path})
